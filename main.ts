f6bbf68c198a2c03cca5b4868bc73f17d3fd14ad
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-07-07 17:21:06 -0400

    Jumping and walking

--- main.ts
+++ main.ts
@@ -1,1 +1,40 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    jump_character()
+})
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (sprite_player.isHittingTile(CollisionDirection.Bottom)) {
+        player_jumping = false
+        if (sprite.isHittingTile(CollisionDirection.Left)) {
+            spriteutils.jumpImpulse(sprite, tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.TileWidth) + 2)
+        }
+    }
+})
+function jump_character () {
+    if (!(player_jumping)) {
+        spriteutils.jumpImpulse(sprite_player, 18)
+        player_jumping = true
+    }
+}
+function make_character () {
+    sprite_player = sprites.create(assets.image`player_image`, SpriteKind.Player)
+    sprite_player.ay = GRAVITY
+    enable_controls(true)
+}
+function enable_controls (en: boolean) {
+    if (en) {
+        controller.moveSprite(sprite_player, MOVE_SPEED, 0)
+    } else {
+        controller.moveSprite(sprite_player, 0, 0)
+    }
+}
+let player_jumping = false
+let sprite_player: Sprite = null
+let GRAVITY = 0
+let MOVE_SPEED = 0
+stats.turnStats(true)
+MOVE_SPEED = 100
+GRAVITY = 500
+let tilemap_map = [[tileUtil.createSmallMap(tilemap`level2`)]]
+make_character()
+scene.setBackgroundColor(12)
+tiles.setCurrentTilemap(tilemap_map[0][0])
---
+++ tilemap.g.ts
@@ -0,0 +1,107 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "map_0_0":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010100000000000000020202020202020202020000000000000202020202020202020202020200000002020202020202020202020202020200020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile2,myTiles.tile3], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`14000f00040404040404040403030303030303030303030304040404040404040303030303030303030303030404040404040404040303030303030303030303040404040404040404000003030303030303030304040404040404040400000000000303030303030404040404040404040000000000000000030303010101010101010101000000000000000000030302020202020202020202000000000000000003030202020202020202020202020000000500000003020202020202020202020202020000000000000002030303030202020202020202020000000000000303030303030302020203030202020200000500030303030303030303030303030303030000000003030303030303030303030303030303030000000303030303030303030303030303030303030303`, img`
+. . . . . . . . 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . 2 2 2 
+2 2 2 2 2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 2 2 2 2 2 . . . . . . . . 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency8,myTiles.tile1,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile8], TileScale.Eight);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "grass_top":
+            case "tile2":return tile2;
+            case "dirt":
+            case "tile3":return tile3;
+            case "transparency8":return transparency8;
+            case "rock":
+            case "tile5":return tile5;
+            case "sky":
+            case "tile6":return tile6;
+            case "dirt0":
+            case "tile4":return tile4;
+            case "grass_top0":
+            case "tile1":return tile1;
+            case "torch":
+            case "tile8":return tile8;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
