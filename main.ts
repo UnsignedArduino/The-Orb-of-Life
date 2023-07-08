21573a591280060b2a58bb823a50775328559747
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-07-07 23:14:30 -0400

    Horizontal map transitions

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+function location_index (loc_in_list: tiles.Location[], locs: any[]) {
+    for (let index = 0; index <= locs.length - 1; index++) {
+        if (loc_in_list[0].column == locs[index].column && loc_in_list[0].row == locs[index].row) {
+            return index
+        }
+    }
+    return -1
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     jump_character()
 })
@@ -15,6 +23,17 @@ function jump_character () {
         player_jumping = true
     }
 }
+function load_map () {
+    tiles.setCurrentTilemap(game_map[map_y][map_x])
+    tiles.placeOnRandomTile(sprite_player, assets.tile`spawn_in_tile`)
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`to_right_map`, function (sprite, location) {
+    local_overlap_index = location_index([location], tiles.getTilesByType(assets.tile`to_right_map`))
+    map_x += 1
+    load_map()
+    tiles.placeOnTile(sprite, tiles.getTilesByType(assets.tile`to_left_map`)[local_overlap_index])
+    sprite.x += tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.TileWidth)
+})
 function make_character () {
     sprite_player = sprites.create(assets.image`player_image`, SpriteKind.Player)
     sprite_player.ay = GRAVITY
@@ -27,14 +46,28 @@ function enable_controls (en: boolean) {
         controller.moveSprite(sprite_player, 0, 0)
     }
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`to_left_map`, function (sprite, location) {
+    local_overlap_index = location_index([location], tiles.getTilesByType(assets.tile`to_left_map`))
+    map_x += -1
+    load_map()
+    tiles.placeOnTile(sprite, tiles.getTilesByType(assets.tile`to_right_map`)[local_overlap_index])
+    sprite.x += tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.TileWidth) * -1
+})
+let local_overlap_index = 0
 let player_jumping = false
 let sprite_player: Sprite = null
+let map_y = 0
+let map_x = 0
+let game_map: tiles.TileMapData[][] = []
 let GRAVITY = 0
 let MOVE_SPEED = 0
 stats.turnStats(true)
 MOVE_SPEED = 100
 GRAVITY = 500
-let tilemap_map = [[tileUtil.createSmallMap(tilemap`level2`)]]
+game_map = [[tileUtil.createSmallMap(tilemap`level2`), tileUtil.createSmallMap(tilemap`level4`)]]
+map_x = 0
+map_y = 0
 make_character()
 scene.setBackgroundColor(12)
-tiles.setCurrentTilemap(tilemap_map[0][0])
+load_map()
+scene.centerCameraAt(scene.cameraProperty(CameraProperty.X) + tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.TileWidth), scene.cameraProperty(CameraProperty.Y) + tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.TileWidth))
